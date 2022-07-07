2b315e7e01db379a88f037048f6d5c4d516b3280
zaid.azaize <zaid.azaize@gmail.com>
2022-07-07 23:56:18 +0530

    gradle upgrade to v 7.3

---
+++ app/src/main/java/com/tentlers/mngapp/LaunchScreen.kt
@@ -0,0 +1,11 @@
+package com.tentlers.mngapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LaunchScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch_screen)
+    }
+}
