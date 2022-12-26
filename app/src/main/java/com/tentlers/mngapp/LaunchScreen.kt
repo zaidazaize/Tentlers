982e47c752f4ef5c3a83d3727d1a14e45423f122
Zaid Aziz <67843799+zaidazaize@users.noreply.github.com>
2022-12-27 02:33:10 +0530

    splash screen

--- app/src/main/java/com/tentlers/mngapp/LaunchScreen.kt
+++ app/src/main/java/com/tentlers/mngapp/LaunchScreen.kt
@@ -1,11 +1,20 @@
 package com.tentlers.mngapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class LaunchScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch_screen)
+
+//        TODO: change the splash screen method to not use handler() object.
+        Handler().postDelayed(Runnable { startActivity(Intent(this, MainActivity::class.java))
+            finish() },2000)
+
     }
 }
